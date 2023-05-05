b5a43941a33e952c728951842fdb504a42c636a0
clk-99 <64612452+clk-99@users.noreply.github.com>
2023-05-05 15:49:18 +0200

    Update prep_data.py

--- Scripts/prep_data.py
+++ Scripts/prep_data.py
@@ -16,14 +16,11 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import os
-#print(os.listdir("../Data/ADULT_CENSUS_INCOME/"))
 data_folder = '../Data'
 
 def main(data_path,dataset,cat_columns):
     df = load_dataset(data_path,cat_columns)
-    df, cat_vars, num_vars, target = dataframe_investigation(df, dataset)
-    print(num_vars)
-    print(cat_vars)
+    df, target = dataframe_investigation(df, dataset)
     target_df = df[target]
     features_df = df[cat_vars+num_vars]
     if dataset == 'wine':
@@ -35,14 +32,14 @@ def main(data_path,dataset,cat_columns):
 
     elif dataset == 'bank':
         X_train, X_test, y_train, y_test = train_test_split(features_df,target_df,test_size=0.20,random_state=12)
-        X_train, X_test = feature_engineering(X_train, X_test, dataset, cat_vars=cat_vars)
+        X_train, X_test = feature_engineering(X_train, X_test, dataset, cat_vars=cat_columns)
         real_train_data = pd.concat([X_train.reset_index(drop=True),
                                         y_train.reset_index(drop=True)],axis=1)
-        real_train_data.to_csv(data_folder + '/BANK_CUSTOMER_CHURN/BANK_TRAIN_SET.csv',index_label='Index')
+        real_train_data.to_csv(data_folder + '/BANK_MARKETING/BANK_TRAIN_SET.csv',index_label='Index')
     
     elif dataset == 'adult':
         X_train, X_test, y_train, y_test = train_test_split(features_df,target_df,test_size=0.20,random_state=12)
-        X_train, X_test = feature_engineering(X_train, X_test, dataset, cat_vars=cat_vars)
+        X_train, X_test = feature_engineering(X_train, X_test, dataset, cat_vars=cat_columns)
         real_train_data = pd.concat([X_train.reset_index(drop=True),
                                         y_train.reset_index(drop=True)],axis=1)
         real_train_data.to_csv(data_folder + '/ADULT_CENSUS_INCOME/ADULT_TRAIN_SET.csv',index_label='Index')
@@ -52,9 +49,8 @@ def main(data_path,dataset,cat_columns):
         real_train_data = pd.concat([X_train.reset_index(drop=True),
                                         y_train.reset_index(drop=True)],axis=1)
         real_train_data.to_csv(data_folder + '/HEART_ATTACK_PREDICTION/HEART_TRAIN_SET.csv',index_label='Index')
-        #real_train_data.columns = df.columns.to_list()
     
-    return real_train_data, target, num_vars, cat_vars
+    return real_train_data, target
         
   
 
@@ -88,16 +84,11 @@ def dataframe_investigation(df,dataset):
         df[df =='?'] = np.nan
         #impute missing values with mode
         df = check_null_values(df)
-        target_var = 'income'
-        feat_df_only = df.loc[:,df.columns!=target_var]
+        target_var = 'income'   
 
-        cat_vars = ds.structdata.get_cat_feats(feat_df_only)
-        num_vars = ds.structdata.get_num_feats(feat_df_only)
-
-        return df,cat_vars,num_vars,target_var
+        return df,target_var
 
     elif dataset == 'heart':
-        print(df['thal'].unique())
         target_var = 'target'
         df_plot = df.copy()
         di = {'1': 'Male', '0': 'Female'}
@@ -107,38 +98,23 @@ def dataframe_investigation(df,dataset):
         df_plot['sex'].replace(di,inplace=True)
         df_plot['thal'].replace(dj,inplace=True)
         df_plot['target'].replace(dk,inplace=True)
-
-        feat_df_only = df_plot.loc[:,df_plot.columns!=target_var]
-
-        cat_vars = ds.structdata.get_cat_feats(feat_df_only)
-        num_vars = ds.structdata.get_num_feats(feat_df_only)
-
-        print(df_plot['sex'].unique())
-        print(df_plot['thal'].unique())
-        return df_plot,cat_vars,num_vars,target_var
+       
+        return df_plot,target_var
     
     elif dataset == 'bank':
-        df.drop('customer_id',axis=1,inplace=True)
-        target_var = 'churn'
-        feat_df_only = df.loc[:,df.columns!=target_var]       
-        cat_vars = ds.structdata.get_cat_feats(feat_df_only)
-        num_vars = ds.structdata.get_num_feats(feat_df_only)
+        target_var = 'y'
         #impute missing values with mode
         df = check_null_values(df)
 
-        return df,cat_vars,num_vars,target_var
+        return df,target_var
         
     
     else: #wine
         target_var = 'quality'
-        feat_df_only = df.loc[:,df.columns!=target_var]
-        
-        cat_vars = ds.structdata.get_cat_feats(feat_df_only)
-        num_vars = ds.structdata.get_num_feats(feat_df_only)
         #impute missing values with mode
         df = check_null_values(df)
 
-        return df,cat_vars,num_vars,target_var
+        return df,target_var
     
 
 def feature_engineering(train_df=None, test_df=None, dataset='wine', cat_vars=None):
@@ -149,18 +125,11 @@ def feature_engineering(train_df=None, test_df=None, dataset='wine', cat_vars=No
         train_df[feature].replace(train_df[feature].unique(),[i for i in range(train_df[feature].nunique())],inplace=True)
         return train_df
 
-    elif dataset == 'adult':
-        for feature in cat_vars:
-            le = preprocessing.LabelEncoder()
-            train_df[feature] = le.fit_transform(train_df[feature])
-            test_df[feature] = le.transform(test_df[feature])
-        
-        return train_df,test_df
     
     elif dataset == 'heart':
         return print('Not needed')
     
-    else: #bank dataset
+    else: #bank and adult dataset
         for feature in cat_vars:
             le = preprocessing.LabelEncoder()
             train_df[feature] = le.fit_transform(train_df[feature])
