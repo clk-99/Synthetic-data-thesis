612866cde99da4276c2678331330cf99076b02c5
clk-99 <64612452+clk-99@users.noreply.github.com>
2023-05-05 16:54:50 +0200

    Update load_ARF.R

--- Scripts/load_ARF.R
+++ Scripts/load_ARF.R
@@ -1,26 +1,14 @@
 library(arf)
 
-load_arf <- function(df,output_path,saved_model){
+load_arf <- function(df,output_path,cat_vars,saved_model){
   setwd(output_path)
-  for (i in 1:ncol(df)){
-    if(is.character(df[,i])){
-      df[,i]=factor(df[,i])
-      
-    }
-    else if(is.integer(df[,i])){
-      min_max_values <- range(df[,i])
-      if(min_max_values[2]<=4){
-        df[,i]=factor(df[,i])
-        
-      }else{
-        df[,i]=as.numeric(df[,i])
-        
-      }
-    }
-  }
+ 
   params <- readRDS(saved_model)
  # params <- forde(arf,df)
   synth <- forge(params, n_synth = length(df))
-  
+    
+  for (col in cat_vars){
+     synth$col <- as.character(synth$col)
+    }
   synth
 }
