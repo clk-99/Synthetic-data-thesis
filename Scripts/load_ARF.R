23088ffa3b9fc87680219c3f7864b9a4f06874cb
clk-99 <64612452+clk-99@users.noreply.github.com>
2023-05-05 17:07:42 +0200

    Update load_ARF.R

--- Scripts/load_ARF.R
+++ Scripts/load_ARF.R
@@ -7,8 +7,10 @@ load_arf <- function(df,output_path,cat_vars,saved_model){
  # params <- forde(arf,df)
   synth <- forge(params, n_synth = length(df))
     
-  for (col in cat_vars){
-     synth$col <- as.character(synth$col)
+  for(i in colnames(df)){
+    if(i %in% cat_vars){
+    synth[,i] <- as.character(synth[,i)
     }
+  }
   synth
 }
