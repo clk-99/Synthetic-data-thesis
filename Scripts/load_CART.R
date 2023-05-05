0f1004aa7c35024f60457adfdc11906d69f0cb35
clk-99 <64612452+clk-99@users.noreply.github.com>
2023-05-05 16:57:13 +0200

    Update load_CART.R

--- Scripts/load_CART.R
+++ Scripts/load_CART.R
@@ -1,10 +1,14 @@
 library(synthpop)
 
-load_cart <- function(df,output_path,saved_model){
+load_cart <- function(df,output_path,cat_columns,saved_model){
   setwd(output_path)
   cart <- readRDS(saved_model)
   
   synth <- cart$syn
   
-  syn
+  for(col in cat_columns){
+    synth$col <- as.character(synth$col)
+  }
+  
+  synth
 }
